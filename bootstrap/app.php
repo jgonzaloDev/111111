3716ac372e77de376a0f664e6e92cfcc58f87f00
jgonzaloDev <quinonesramirezjuniorgonzalo@gmail.com>
2025-04-20 21:17:52 -0500

    app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,17 +1,19 @@
 <?php
 
+use App\Http\Middleware\CorsMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+return Application::configure(basePath: dirname(_DIR_))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: _DIR_.'/../routes/web.php',
+        commands: _DIR_.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->validateCsrfTokens(except:['alumnos','alumnos/*']);
+        $middleware->append(CorsMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
