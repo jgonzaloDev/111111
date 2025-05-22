d7434c77fe04ddd6fde7a61e73e695e50b48f84d
jgonzaloDev <quinonesramirezjuniorgonzalo@gmail.com>
2025-05-22 00:18:58 -0500

    cors.php

---
+++ config/cors.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    'paths' => ['api/*', 'alumnos', '*'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'https://prueba1-h7ghbxdmbeeybf5.eastus2-01.azurestaticapps.net',
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 86400,
+
+    'supports_credentials' => false,
+
+];
